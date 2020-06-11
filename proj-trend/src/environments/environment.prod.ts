9ae6381b154bfa57a2d5118fb8a4cf8febe6ac78
Xzavier Dunn <xmandunn@gmail.com>
2020-06-11 14:59:52 -0400

    upgraded angular

--- proj-trend/src/environments/environment.prod.ts
+++ proj-trend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
 };
 
-// export let apiUrl = 'http://127.0.0.1:5000/';
-export let apiUrl = 'http://finaltest-dev.us-east-1.elasticbeanstalk.com/';
+export let apiUrl = "http://127.0.0.1:5000/";
+// export let apiUrl = 'http://finaltest-dev.us-east-1.elasticbeanstalk.com/';
